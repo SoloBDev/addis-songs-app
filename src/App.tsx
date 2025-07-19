ec4a8473fc073ed344a30cb2b380fb6d75c7eab4
sofiberry <sofi2darling@gmail.com>
2025-07-19 22:27:42 +0300

    set up: manual react + webpack + typescript boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>🎵 Addis Songs App</h1>
+      <p>Welcome! We'll build a full CRUD songs manager.</p>
+    </div>
+  );
+};
+
+export default App;
+
