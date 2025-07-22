fdd8e6deb4fbec3065a95430a5cea5ec97b519d7
sofiberry <sofi2darling@gmail.com>
2025-07-22 08:21:09 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      filename: 'index.html',
-      inject: 'body',
     }),
     new Dotenv()
   ],
